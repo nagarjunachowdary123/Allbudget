f2f743b0602028fdcb9e235bf06c15e0652d0355
nagarjuna <nagarjunajampula@gmail.com>
2023-02-16 13:14:37 +0000

    my 10th commit from virginiagit

---
+++ projectplan.js
@@ -0,0 +1,3 @@
+A project is a temporary effort with a specific goal or objective that is typically completed within a defined timeframe and budget. Projects can vary in scope and complexity, from small-scale tasks that can be completed in a few hours to large-scale initiatives that may take months or years to complete.
+
+Projects typically follow a defined process or methodology to ensure that they are completed on time, within budget, and to the required level of quality. Common project management methodologies include Agile, Waterfall, and Scrum, among others.
